62013bf95410719940c5d074597ba448b40f7768
25424hz <25424hz@hvhs.school.nz>
2026-06-19 15:11:19 +1200

    Basic set up (copied the previous codes)

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+/************************
+Header 
+************************/
 console.log("Hello world!")
+
+
+/************************
+Variables 
+************************/
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+let shoppingList = [];
+
+/************************
+ Main Code
+************************/
+OUTPUT.innerHTML = "This is the output from JavaScript!";
+
+
+/*************************
+Functions 
+************************/ 
+function getFormInput() {
+    const NAME_FIELD = document.getElementById("nameField");
+    let userName = NAME_FIELD.value;
+}
+
+function addShopping(){
+    const SHOP_FIELD = document.getElementById("shopField");
+    OUTPUT.innerHTML +=  "<br>You have added <b>[ " +SHOP_FIELD.value+" ]</b> to the list";
+    shoppingList.push(SHOP_FIELD.value);
+    OUTPUT.innerHTML += "<br>There are <b>" + shoppingList.length + "</b> items in your shopping list.<br>" ;
+    for (let i = 0; i < shoppingList.length; i++) {
+        OUTPUT.innerHTML += (i+1) + ". " + shoppingList[i] + "<br>";
+    } 
+    OUTPUT.innerHTML += "<br>";
+}
