82b4b32ff0b76a37bf786c1b32a4f3777a0beb21
25424hz <25424hz@hvhs.school.nz>
2026-07-23 13:27:57 +1200

    goodGrid!

--- script.js
+++ script.js
@@ -8,40 +8,50 @@ console.log("Hello world!")
 Variables 
 ************************/
 const OUTPUT = document.getElementById("JavaScript");
+const WELCOME_FORM = document.getElementById("welcomeForm");
 let shoppingList = [];
 let MUFFIN_LIST = ["plain", "chocolate", "blueberry", "banana", "strawberry", "cinnamon"];
 let CROISSANT_LIST = ["chocolate", "almond", "plain", "cheese", "ham"];
 let COFFEE_LIST = ["latte", "cappuccino", "americano", "espresso"];
 let FRUIT_WINE_LIST = ["apple", "grape", "passionfruit", "fig", "grapefruit"];
 
-const GOOD_MUFFIN = document.getElementById("goodMuffin");
-const GOOD_CROISSANT = document.getElementById("goodCroissant");
-const GOOD_COFFEE = document.getElementById("goodCoffee");
-const GOOD_FRUIT_WINE = document.getElementById("goodFruitWine");
+
 /************************
  Main Code
 ************************/
-OUTPUT.innerHTML = "<h2>Best Recommended! ↖(^ω^)↗</h2>";
-GOOD_MUFFIN.innerHTML
-GOOD_CROISSANT.innerHTML
-GOOD_COFFEE.innerHTML
-GOOD_FRUIT_WINE.innerHTML
+OUTPUT.innerHTML += "<h2 class='title'>Best Recommended! ↖(^ω^)↗</h2>";
+OUTPUT.innerHTML += "<img src='Images/coffee/latte.jpeg' id='coffee4' class='halfWidth' alt='latte'>";
+OUTPUT.innerHTML += "<img src='Images/croissant/ham&cheese.jpg' id='croissant5' class='halfWidth' alt='ham&cheese'>";
+OUTPUT.innerHTML += "<img src='Images/muffin/choc.jpg' id='goodMuffin' class='halfWidth' alt='goodMuffin'>";
+OUTPUT.innerHTML += "<img src='Images/wine/apple.png' id='goodFruitWine' class='halfWidth' alt='goodFruitWine'>";
+OUTPUT.innerHTML += "<h3 class='foodName'>Latte - Morning Mist</h3>";
+OUTPUT.innerHTML += "<h3 class='foodName'>Croissant - Morning Savour</h3>";
+OUTPUT.innerHTML += "<h3 class='foodName'>Muffin - Midnight Cocoa</h3>";
+OUTPUT.innerHTML += "<h3 class='foodName'>Fruit Wine - Golden Orchard</h3>";
+OUTPUT.innerHTML += "<p class='description'>Smooth espresso blended with silky steamed milk, creating a rich, creamy coffee with a velvety finish.</p>";
+OUTPUT.innerHTML += "<p class='description'>Tender smoked ham nestled in buttery pastry for a warm and satisfying savoury bite.</p>";
+OUTPUT.innerHTML += "<p class='description'>Rich cocoa and melted chocolate folded into a moist muffin for an indulgent treat.</p>";
+OUTPUT.innerHTML += "<p class='description'>Light and refreshing with crisp apple notes and a smooth, delicate finish.</p>";
 
 /*************************
 Functions 
 ************************/
 function nameInput() {
     const NAME_FIELD = document.getElementById("nameField");
-    if (NAME_FIELD.checkValidity === false) {
-        OUTPUT.innerHTML = "<br>Sorry, please enter a valid name.";
+    if (NAME_FIELD.value.trim() === "") {
+        return;
+    } else if (NAME_FIELD.value.trim().length < 3) {
+        WELCOME_FORM.innerHTML += "<br><p>Your name must be at least 3 characters long.</p>";
+        return;
     } else {
-        OUTPUT.innerHTML = "<br>Welcome to the Shed of Light, <b>" + NAME_FIELD.value + "</b>!<br>";
+        WELCOME_FORM.innerHTML = "<br>Welcome to the Shed of Light, " + NAME_FIELD.value.trim() + "!<br>";
+        return;
     }
 }
 
 function addShopping() {
     const SHOP_FIELD = document.getElementById("shopField");
-    if (SHOP_FIELD.checkValidity === false) {
+    if (SHOP_FIELD.value === "") {
         OUTPUT.innerHTML += "<br>Sorry, please enter a valid item.";
     } else {
         OUTPUT.innerHTML += "<br>You have added <b>[ " + SHOP_FIELD.value + " ]</b> to the list";
@@ -53,4 +63,3 @@ function addShopping() {
         OUTPUT.innerHTML += "<br>";
     }
 }
-
